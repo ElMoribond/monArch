1840f7acdbf8d76625118b54dcd5688164c620b5
ElMoribond <ElMoribond@users.noreply.github.com>
2019-11-28 12:20:50 +0100

    Update postinstall_a.sh

--- postinstall_a.sh
+++ postinstall_a.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+systemctl enable dnsmasq sshd
+
+if [[ -r /srv/vnstat/vnstat.db ]]; then
+  systemctl enable vnstat
+else
+  echo
+  echo "--------------->>>>>  Penser à copier les données vnstat"
+  echo
+fi
+
+echo "---------------------------- Création utilisateur"
+useradd -m ${MON_USER}
+usermod -aG users,video,audio ${MON_USER}
+cat <<EOT >> /etc/sudoers
+${MON_USER} ALL=(ALL) NOPASSWD: ALL
+EOT
+
+rm -f /home/${MON_USER}/.bash.bashrc
+
