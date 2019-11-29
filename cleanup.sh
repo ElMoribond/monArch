fb7cd9907d9fc94a624fcf5df1b885e68ab5ac0f
ElMoribond <ElMoribond@users.noreply.github.com>
2019-11-29 17:04:16 +0100

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,20 @@
+
+
+pacman -Rsn discover
+
+# Suppression des jeux (paquet fltk indispensable à tigervnc qui est nécessaire à xrdp)
+# et raccourcis FLUID
+rm -f /usr/share/icons/hicolor/128x128/apps/blocks.png
+rm -f /usr/share/icons/hicolor/128x128/apps/checkers.png
+rm -f /usr/share/icons/hicolor/128x128/apps/sudoku.png
+rm -f /usr/share/applications/blocks.desktop
+rm -f /usr/share/applications/checkers.desktop
+rm -f /usr/share/applications/fluid.desktop
+rm -f /usr/share/applications/sudoku.desktop
+rm -f /usr/bin/blocks
+rm -f /usr/bin/checkers
+rm -f /usr/bin/sudoku
+rm -f /usr/share/man/man6/blocks.6.gz
+rm -f /usr/share/man/man6/checkers.6.gz
+rm -f /usr/share/man/man6/sudoku.6.gz
+
