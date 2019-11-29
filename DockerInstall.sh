99b940b933c8484e763313ce465ff588e9525189
ElMoribond <ElMoribond@users.noreply.github.com>
2019-11-29 15:15:06 +0100

    Create DockerInstall.sh

---
+++ DockerInstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pacman -S docker
+
+mkdir /etc/docker
+cat <<EOF >> /etc/docker/daemon.json
+{
+  "debug": false,
+  "log-driver": "journald",
+  "dns": ["10.0.0.1", "192.168.1.1"],
+  "iptables": false
+}
+EOF
