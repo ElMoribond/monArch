dd4d04925f57df9b71b0f482908ac68627eca8f4
ElMoribond <ElMoribond@users.noreply.github.com>
2019-11-29 09:25:07 +0100

    Update postinstall_a.sh

--- postinstall_a.sh
+++ postinstall_a.sh
@@ -14,12 +14,20 @@ else
   echo "--------------->>>>>  Penser à copier les données vnstat"
   echo
 fi
+sed -i 's/Interface ""/Interface "WAN LAN WIFI"/g' /etc/vnstat.conf
+sed -i 's/DatabaseDir \"\/var\/lib\/vnstat\"/DatabaseDir \"\/srv\/vnstat\"/g' /etc/vnstat.conf
+sed -i 's/ExecStart=\/usr\/sbin\/vnstatd/ExecStart=\/usr\/bin\/vnstatd/g' /usr/lib/systemd/system/vnstat.service
 
 echo "---------------------------- Création utilisateur"
 useradd -m ${MON_USER}
 usermod -aG users,video,audio ${MON_USER}
 cat <<EOT >> /etc/sudoers
 
+userdel ftp
+rm -rf /srv/ftp
+
 ${MON_USER} ALL=(ALL) NOPASSWD: ALL
 EOT
 rm -f /home/${MON_USER}/.bash.bashrc
+
+pacman -Syu docker mosquitto 
