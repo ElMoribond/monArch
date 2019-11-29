92c8fe9962ba94f3d99ec5f5ff5d8a4c41a9a9f3
ElMoribond <ElMoribond@users.noreply.github.com>
2019-11-29 07:56:11 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -3,13 +3,8 @@
 echo "---------------------------- Installation de grub"
 mount | grep efivars &> /dev/null || mount -t efivarfs efivarfs/sys/firmware/efi/efivars
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
-
-ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-
-echo "---------------------------- Configuration de grub"
 sed -i '/GRUB_TIMEOUT/c\GRUB_TIMEOUT=1' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-pause
 
 echo "---------------------------- Configuration langue"
 cat <<EOT >> /etc/locale.conf
@@ -21,20 +16,16 @@ EOT
 sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
 sed -i "s/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/g" /etc/locale.gen
 locale-gen
-pause
 
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 sed -i "s/DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=10s/g" /etc/systemd/system.conf 
-
 rm -f /root/.bash.bashrc
 
-echo "---------------------------- Vérification langue"
-wget -O - https://raw.githubusercontent.com/ElMoribond/monArch/master/locale-check.sh | bash
-
-echo
 echo
 echo "----------------------------"
 echo "Penser au mot de passe user et root"
 echo
 echo "Prochain script https://tinyurl.com/MonPostinstall02"
 echo
-echo
+echo "Activer services, au minimum:  systemd-networkd, dnsmasq et sshd"
+echo "Vérifier avec networkctl et systemctl"
