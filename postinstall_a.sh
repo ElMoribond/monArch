79e676b63b3cd917ee271bc2820f8f3c9c4c8577
ElMoribond <ElMoribond@users.noreply.github.com>
2019-11-29 08:44:06 +0100

    Update postinstall_a.sh

--- postinstall_a.sh
+++ postinstall_a.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-systemctl enable dnsmasq sshd
+if [[ -z ${MON_USER+x} ]]; then
+  echo "Variable MON_USER indéfinie !"
+  exit 1
+fi
+
+systemctl enable systemd-networkd.service dnsmasq.service sshd.service hostapd.service
 
 if [[ -r /srv/vnstat/vnstat.db ]]; then
-  systemctl enable vnstat
+  systemctl enable vnstat.service
 else
   echo
   echo "--------------->>>>>  Penser à copier les données vnstat"
@@ -16,6 +21,4 @@ usermod -aG users,video,audio ${MON_USER}
 cat <<EOT >> /etc/sudoers
 ${MON_USER} ALL=(ALL) NOPASSWD: ALL
 EOT
-
 rm -f /home/${MON_USER}/.bash.bashrc
-
